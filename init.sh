31ded363e9309b55f2b13b0c37e2df2ed152beed
Jose Angel Espinoza Portillo <jose@ngel.es>
2019-10-24 01:44:58 -0500

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Simple script to install AppScale and tools from the master branch
+# Author: AppScale Team <support@appscale.com>
+
+HOME="$(pwd)"
+sudo -i
+
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
+
+echo "$HOME/.ssh/id_rsa.pub" >> /root/.ssh/authorized_keys
+
+wget https://raw.githubusercontent.com/crypt0cloud/appscale/master/bootstrap.sh
+
+bash bootstrap.sh
