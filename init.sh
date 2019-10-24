26f6dc728593b67262d4687c4f703486cf477b35
Jose Angel Espinoza Portillo <jose@ngel.es>
2019-10-24 02:55:08 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@
 # Simple script to install AppScale and tools from the master branch
 # Author: AppScale Team <support@appscale.com>
 
-HOME="$(pwd)"
-echo "Path origen: $HOME"
+HOMEstr="$(pwd)"
+echo "Path origen: $HOMEstr"
 
 
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
 
-echo "$HOME/.ssh/id_rsa.pub" >> /root/.ssh/authorized_keys
+echo "$HOMEstr/.ssh/id_rsa.pub" >> /root/.ssh/authorized_keys
 
 wget https://raw.githubusercontent.com/crypt0cloud/appscale/master/bootstrap.sh -O bootstrap.sh
 
