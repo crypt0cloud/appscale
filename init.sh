25d27ea187cfdf45f77c63c3ae49b55cd4d0dad4
Jose Angel Espinoza Portillo <jose@ngel.es>
2019-10-24 02:32:04 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,15 +6,6 @@
 HOME="$(pwd)"
 echo "Path origen: $HOME"
 
-whoami
-sudo -i -u root bash << EOF
-echo "In"
-whoami
-EOF
-echo "Out"
-whoami
-
-pwd
 
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
 
@@ -22,4 +13,4 @@ echo "$HOME/.ssh/id_rsa.pub" >> /root/.ssh/authorized_keys
 
 wget https://raw.githubusercontent.com/crypt0cloud/appscale/master/bootstrap.sh -O bootstrap.sh
 
-bash bootstrap.sh
+sh bootstrap.sh
